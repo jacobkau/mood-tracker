2ab7da3f42c6e700b795e8ed51b746f1a3b98bc0
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-08-02 15:45:19 +0300

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -54,6 +54,7 @@ export default function Navbar({ setIsAuthenticated }) {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           {/* Logo */}
+
           <div className="flex items-center">
   <Link
     to="/"
