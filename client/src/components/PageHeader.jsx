7ea58bedb9702cfaf76521932cd7a13638ff255a
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-09-02 15:42:13 +0300

    Update PageHeader.jsx

--- client/src/components/PageHeader.jsx
+++ client/src/components/PageHeader.jsx
@@ -1,7 +1,10 @@
-
+import { useTheme } from '../context/useTheme';
 const PageHeader = ({ title, description }) => {
+  const { theme, themes } = useTheme();
+  const currentTheme = themes[theme];
+  
   return (
-    <div className="bg-gradient-to-r from-indigo-600 to-purple-600 py-12">
+    <div className={'${currentTheme.footerBg} ${currentTheme.footerText} bg-gradient-to-r from-indigo-600 to-purple-600 py-12'}>
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h1 className="text-3xl md:text-4xl font-bold text-white">{title}</h1>
         {description && (
