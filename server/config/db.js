4fe97a7b047bf745872b6b3e47bd9f3e0704396e
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-09-02 17:12:23 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 30000,
       maxPoolSize: 10,
