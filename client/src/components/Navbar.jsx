3add4a8d7481c6cea597e1243bd905b634b2e852
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-09-02 13:57:18 +0300

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -85,8 +85,8 @@ export default function Navbar({ setIsAuthenticated, isAuthenticated }) {
     };
 
     return (
-        <nav className={`${currentTheme.navBg} shadow-lg`}>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <nav className={`sticky top-0 z-50 ${currentTheme.navBg} shadow-lg`}>
+  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     {/* Logo */}
                     <div className="flex items-center">
