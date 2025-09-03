129d9b6f710ffb9881208d661e7e117de120a8fa
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-09-03 14:19:28 +0300

    Update MoodList.jsx

--- client/src/components/mood/MoodList.jsx
+++ client/src/components/mood/MoodList.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import { useTheme } from "../../context/UseTheme";
+import { useTheme } from "../../context/useTheme';
 
 
 export default function MoodList({ moods, setMoods }) {
