7167f73c60ad2ca3e9c2a3f08af6987cffe7a97e
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-09-03 17:01:31 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -9,6 +9,14 @@ const UserSchema = new mongoose.Schema({
     trim: true,
     minlength: 3
   },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/.+@.+\..+/, "Please enter a valid email address"] // basic validation
+  },
   password: {
     type: String,
     required: [true, "Password is required"],
@@ -16,6 +24,7 @@ const UserSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
+// hash password before saving
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
