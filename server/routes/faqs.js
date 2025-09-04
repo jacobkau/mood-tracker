4de9bdc511d538b0504db7b8e407f50f9d3210d5
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-09-04 21:44:44 +0300

    Update faqs.js

--- server/routes/faqs.js
+++ server/routes/faqs.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const FAQ = require('../models/FAQ');
-const auth = require('../middleware/auth');
-const adminAuth = require('../middleware/adminAuth');
+const { protect, admin } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 // Get all FAQs
