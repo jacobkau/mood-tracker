0d8fd79c2bf6dd8c805381dc6cd5306c6546c726
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-09-04 18:22:31 +0300

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,6 +10,7 @@ const path = require('path');
 const fs = require('fs');
 const crypto = require("crypto");
 
+
 // IMAGE UPLOAD CONFIGURATION
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
